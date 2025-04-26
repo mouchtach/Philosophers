02ca0c6462a0398bf5748a5f11c9ea958853ae0b
youssef <youssefmouchtach674@gmail.com>
2025-04-26 05:18:54 +0100

    ss

---
+++ philo.h
@@ -0,0 +1,44 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <limits.h>
+
+typedef  pthread_mutex_t t_mtx;
+
+typedef struct s_fork
+{
+    t_mtx   fork;
+    int     fork_id;
+}t_fork;
+
+typedef struct s_philo
+{
+    int         id;
+    long        meals_counter;
+    bool        full;
+    long        last_meal_time;
+    t_fork      *left_fork;
+    t_fork      *right_fork;
+    pthread_t    thread_id;
+}t_philo;
+
+
+typedef struct s_table
+{
+    long    philo_number;
+    long    time_to_die;
+    long    time_to_eat;
+    long    time_to_sleep;
+    long    nbr_limit_meals;
+    long    start_simulation;
+    bool    end_simulation;
+    t_fork  *forks;
+    t_philo *philos;
+}t_table;
+#endif
